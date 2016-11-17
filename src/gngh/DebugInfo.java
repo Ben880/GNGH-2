b75265ee93c1fd0ef0990e31cbc7499edb8b7eb1
Ben880 <wilcoxben11@gmail.com>
2016-11-17 00:26:32 -0500

    Added DebugInfo to store debug status

---
+++ src/gngh/DebugInfo.java
@@ -0,0 +1,25 @@
+package gngh;
+
+/*
+    BenjaminWilcox
+    Nov 17, 2016
+    GNGH_2
+ */
+public class DebugInfo
+{
+
+    private boolean debug = false;
+
+    public void toggleDebug()
+    {
+        if (debug)
+            debug = false;
+        else
+            debug = true;
+    }
+
+    public boolean getDebug()
+    {
+        return debug;
+    }
+}
