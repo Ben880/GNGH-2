930ad2f02421cced93106c36a44c99e2db7e0fb8
Ben880 <wilcoxben11@gmail.com>
2016-11-30 20:40:18 -0500

    Base of new enemy system

---
+++ src/enemy/Enemy.java
@@ -0,0 +1,21 @@
+package enemy;
+
+/*
+    BenjaminWilcox
+    Nov 30, 2016
+    GNGH_2
+ */
+public class Enemy
+{
+
+    int type;
+    int skill;
+    int amount;
+
+    public Enemy(int t, int s, int a)
+    {
+        type = t;
+        skill = s;
+        amount = a;
+    }
+}
