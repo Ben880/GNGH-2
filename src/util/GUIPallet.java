92614dfb492cc226ac778ff450bd7cc340e5e948
Ben880 <wilcoxben11@gmail.com>
2016-12-22 01:38:05 -0500

    added colors

--- src/util/GUIPallet.java
+++ src/util/GUIPallet.java
@@ -1,10 +1,26 @@
 package util;
 
+import java.awt.Color;
+
 /*
     BenjaminWilcox
     Nov 30, 2016
     GNGH_2
  */
-public class GUIPallet {
+public class GUIPallet
+{
 
+    Color background = new Color(100, 100, 100);
+    Color panel = new Color(170, 170, 170);
+    Color myGrey = new Color(225, 225, 225);
+
+    public Color getPanel()
+    {
+        return panel;
+    }
+
+    public Color getBackground()
+    {
+        return background;
+    }
 }
