1a99140dabad44a5e5ef9bd6b3a493dd9150f5bb
Ben880 <wilcoxben11@gmail.com>
2017-06-13 16:18:35 -0400

    imported my file loader

---
+++ src/CustomFileLoader/FileFormatException.java
@@ -0,0 +1,15 @@
+package CustomFileLoader;
+
+/*
+    BenjaminWilcox
+    May 31, 2017
+    FileLoader
+ */
+class FileFormatException extends Exception
+{
+
+    public FileFormatException()
+    {
+    }
+
+}
